192eb20294eaa55e73397d97de985e2335ec1298
taniten <taniten.physics@gmail.com>
2020-11-10 11:06:23 +0900

    Minor changes

--- Analysis.py
+++ Analysis.py
@@ -5,10 +5,4 @@ import os
 import copy
 import time
 
-def readcsv(data_path):
-  data_ndarray = np.loadtxt(data_path + ".csv", delimiter=",")
-  print("read : " + data_path + ".csv")
-  return data_ndarray
-
-readcsv()
 
